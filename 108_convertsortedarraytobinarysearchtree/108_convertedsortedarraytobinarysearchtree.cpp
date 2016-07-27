bc77b08bf703232b9beda99042b66fe5d2e212c4
zusongpeng <zusongpeng@gmail.com>
2016-07-27 09:48:18 +0800

    Add 108_convertsortedarraytobinarysearchtree.

---
+++ 108_convertsortedarraytobinarysearchtree/108_convertedsortedarraytobinarysearchtree.cpp
@@ -0,0 +1,37 @@
+// Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+
+// Author: Songpeng Zu
+// Time: July 26, 2016
+
+#include<vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+// Solver 1
+// From Discussion
+
+class Solution {
+public:
+  TreeNode* sortedArrayToBST(vector<int>& nums) {
+    if (nums.size() == 0) {return NULL;}
+    if (nums.size() == 1) {return new TreeNode(nums[0]);}
+    int middle = nums.size()/2;
+    TreeNode * root = new TreeNode(nums[middle]);
+    vector<int> left(nums.begin(), nums.begin() + middle);
+    vector<int> right(nums.begin()+middle + 1, nums.end());
+    root->left = sortedArrayToBST(left);
+    root->right = sortedArrayToBST(right);
+    return root;
+  }
+};
